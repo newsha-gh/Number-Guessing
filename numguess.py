6bcdaa6150f30581a48b1d9fa65b56919b397e0e
newsha-gh <44205024+newsha-gh@users.noreply.github.com>
2025-07-15 13:16:13 +0200

    numguess.py

--- numguess.py
+++ numguess.py
@@ -1,7 +1,18 @@
 import random
-numberguess=int(input("Enter a number between 1 and 10: "))
-result=random.randint(0,10)
-if result == numberguess:
-    print("Correct! You guessed it.")
+guess_number = random.randint(1, 10)
+
+print("Guess a number between 1 and 10.")
+print("You have 5 tries to guess the right number.")
+
+for attempt in range(1, 6):
+    guess = int(input(f"Try #{attempt}: "))
+
+    if guess == guess_number:
+        print("Correct! You guessed it.")
+        break
+    elif guess < guess_number:
+        print("Too low.")
+    else:
+        print("Too high.")
 else:
-    print(f"Wrong. The correct number was {result}.")
+    print(f"Sorry, you're out of tries. The number was {guess_number}.")
